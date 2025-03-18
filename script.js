d0909c8a7d7ac7a0b030a803f689e17b0ace233c
Sherman Devers <splitsonmyd@gmail.com>
2025-03-18 14:14:28 -0500

    Removed the alert for invalid input and just made 0 the default instead.

--- script.js
+++ script.js
@@ -5,77 +5,73 @@ let seconds;
 let secondsRemaining;
 let originalSecondsRemaining;
 let newTime;
-const timerElement = document.querySelector('.timerText');
+const timerElement = document.querySelector(".timerText");
 
 function readInput() {
-    let alerted = false;
-    hours = parseInt(document.querySelector('#hours').value);
-    if (isNaN(hours)) {
-        alert("Please enter a numerical value for all inputs");
-        alerted = true;
-    }
+  let alerted = false;
+  hours = parseInt(document.querySelector("#hours").value);
+  if (isNaN(hours)) {
+    hours = 0;
+  }
 
-    minutes = parseInt(document.querySelector('#minutes').value);
-    if (isNaN(minutes) && alerted == false) { 
-        alert("Please enter a numerical value for all inputs");
-        alerted = true;
-    }
+  minutes = parseInt(document.querySelector("#minutes").value);
+  if (isNaN(minutes) && alerted == false) {
+    minutes = 0;
+  }
 
-    seconds = parseInt(document.querySelector('#seconds').value);
-    if (isNaN(seconds) && alerted == false) {
-        alert("Please enter a numerical value for all inputs");
-        alerted = true;
-    }
+  seconds = parseInt(document.querySelector("#seconds").value);
+  if (isNaN(seconds) && alerted == false) {
+    seconds = 0;
+  }
 
-    secondsRemaining = (hours * 3600) + (minutes * 60) + seconds;
-    originalSecondsRemaining = secondsRemaining;
-    if (alerted == false) {
-        start();
-    }
+  secondsRemaining = hours * 3600 + minutes * 60 + seconds;
+  originalSecondsRemaining = secondsRemaining;
+  if (alerted == false) {
+    start();
+  }
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', updateTimer);
+const button = document.querySelector("button");
+button.addEventListener("click", updateTimer);
 
 function start() {
-
-    timerElement.textContent = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
-    if (timerElement.innerHTML != "00:00:00") {
-        document.getElementsByClassName("timerText")[0].style.color = "white"
-    }
-    timerStart = true;
+  timerElement.textContent = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
+  if (timerElement.innerHTML != "00:00:00") {
+    document.getElementsByClassName("timerText")[0].style.color = "white";
+  }
+  timerStart = true;
 }
 
 function updateTimer() {
-    if (timerStart == true) {
-        if (secondsRemaining === 0) {
-            timerElement.textContent = "Time's up!";
-            document.title = "Time's up!";
-            let alarm = new Audio('alarm/alarm.mp3');
-            alarm.play();
-            return;
-        }
-
-        const hours = Math.floor(secondsRemaining / 3600);
-        const minutes = Math.floor((secondsRemaining / 60) % 60);
-        const seconds = secondsRemaining % 60;
-        newTime = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
-        timerElement.textContent = newTime;
-        document.title = newTime;
-        secondsRemaining--;
-        setTimeout(updateTimer, 1000);
+  if (timerStart == true) {
+    if (secondsRemaining === 0) {
+      timerElement.textContent = "Time's up!";
+      document.title = "Time's up!";
+      let alarm = new Audio("alarm/alarm.mp3");
+      alarm.play();
+      return;
     }
+
+    const hours = Math.floor(secondsRemaining / 3600);
+    const minutes = Math.floor((secondsRemaining / 60) % 60);
+    const seconds = secondsRemaining % 60;
+    newTime = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
+    timerElement.textContent = newTime;
+    document.title = newTime;
+    secondsRemaining--;
+    setTimeout(updateTimer, 1000);
+  }
 }
 
 updateTimer();
 
 function resetTimer() {
-    const timerElement = document.querySelector('.timerText');
-    const hours = Math.floor(originalSecondsRemaining / 3600);
-    const minutes = Math.floor((originalSecondsRemaining / 60) % 60);
-    const seconds = originalSecondsRemaining % 60;
-    newTime = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
-    timerElement.textContent = newTime;
-    document.title = newTime;
-    timerStart = false;
+  const timerElement = document.querySelector(".timerText");
+  const hours = Math.floor(originalSecondsRemaining / 3600);
+  const minutes = Math.floor((originalSecondsRemaining / 60) % 60);
+  const seconds = originalSecondsRemaining % 60;
+  newTime = `${hours.toString().padStart(2, 0)}:${minutes.toString().padStart(2, 0)}:${seconds.toString().padStart(2, 0)}`;
+  timerElement.textContent = newTime;
+  document.title = newTime;
+  timerStart = false;
 }
